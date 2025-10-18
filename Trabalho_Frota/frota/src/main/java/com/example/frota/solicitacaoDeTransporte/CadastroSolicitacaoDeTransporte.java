e37db35779cc7d5700a027dedbdf9835e2fb29f0
Raul Felix <raulfelixsousa@gmail.com>
2025-10-18 10:30:06 -0300

    Refactor CadastroSolicitacaoDeTransporte to record

--- Trabalho_Frota/frota/src/main/java/com/example/frota/solicitacaoDeTransporte/CadastroSolicitacaoDeTransporte.java
+++ Trabalho_Frota/frota/src/main/java/com/example/frota/solicitacaoDeTransporte/CadastroSolicitacaoDeTransporte.java
@@ -1,5 +1,27 @@
 package com.example.frota.solicitacaoDeTransporte;
 
-public class CadastroSolicitacaoDeTransporte {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-}
+public record CadastroSolicitacaoDeTransporte(
+	    @NotBlank(message = "Modelo é obrigatório") 
+	    String nomeProduto,
+	    
+	    @NotNull(message = "Peso é obrigatória")
+	    Double peso,
+	    @NotNull(message = "Largura é obrigatória")
+	    @Positive(message = "Largura deve ser positiva")
+		Double largura,
+
+		@NotNull(message = "Comprimento é obrigatório")
+	    @Positive(message = "Comprimento deve ser positivo")
+		Double comprimento,
+
+		@NotNull(message = "Altura é obrigatória")
+	    @Positive(message = "Altura deve ser positiva")
+		Double altura, 
+		
+	    @NotNull(message = "Caixa é obrigatória")
+	    Long caixaId
+	) {}
